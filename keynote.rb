9c09a00ee580d395f18ff65e5e8bc8bcdeb5d18f
Lisa Kilker <lisakilker@gmail.com>
2015-08-20 12:13:44 +0200

    new changes

--- keynote.rb
+++ keynote.rb
@@ -3,14 +3,20 @@ require 'terminfo'
     class Presentation
     	def initialize(slides_file)
     		@array_of_texts = FileReader.new(slides_file).get_slide_text
+    		@half_screen_height = TermInfo.screen_height / 2
+    		@half_screen_width = TermInfo.screen_width / 2
     	end
     	
     	def display_slides
-    		screen_height = TermInfo.screen_height / 2
-    		screen_width = TermInfo.screen_width / 2
     		@array_of_texts.each do |text|
-    		puts text
+    			print_slide(text)
     		end
+    	end
+
+    	def print_slide(text)
+    		puts "\n" * @half_screen_height
+    		puts ("" * (@half_screen_width - text.length / 2)) + text
+    		puts "\n" * (@half_screen_height - 1)
     	end
     end
 
