13dc6d9a2fea0b9dbb4c236910828edaa2e18b09
Lisa Kilker <lisakilker@gmail.com>
2015-08-20 15:55:14 +0200

    finished

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,23 @@
-when it is a multiple of 3 print 'fizz'
-when it is a multiple of 5 print 'buzz'
-when it is a multiple of 7 print 'blue'
+class Fiz
+	attr_accessor :array
+	def initialize
+		@array = [*1..100]	
+	end
+
+	def run_fizz
+		@array.each do |number|
+			if number % 3 == 0
+		        puts "fizz"
+		    elsif number % 5 == 0
+		        puts "buzz"
+		    elsif number % 7 == 0
+		        puts "blue"
+		    else 
+		    	puts number
+		   end
+		end
+	end
+end
+
+my_fizz = Fiz.new
+my_fizz.run_fizz
