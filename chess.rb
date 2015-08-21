c1b449440b528938b1512be12b0741a812df73bb
Lisa Kilker <lisakilker@gmail.com>
2015-08-21 16:08:52 +0200

    new updates

--- chess.rb
+++ chess.rb
@@ -3,7 +3,7 @@ class Board
 	 	@rows = rows
 	 	@columns = columns
 	 	@@array = Board.new[0,0]
-	 	grid = Array.new(x) {Array.new(y)}
+	 	@@grid = Array.new(x) {Array.new(y)}
 	 end
 end
 
@@ -12,86 +12,57 @@ class Piece
 		@chessboard = chessboard
 	 	@origin = origin
 	 	@destination = destination
-	 	@@moves = IO.readlines("complex_moves.txt")
 	 end
  end
 
 class Pawn < Piece # moves up 1 square or 2 if both are vacant
 	def available_moves(x, y)
-		horz_left x, y
-		horz_right x, y
-		vert_up x, y
-		vert_down x, y
-		moves
+		IO.readlines("simple_moves.txt")
 	end
 end
 
 class Knight < Piece # moves L shaped
 	def available_moves(x, y)
-		horz_left x, y
-		horz_right x, y
-		vert_up x, y
-		vert_down x, y
-		moves
+		IO.readlines("simple_moves.txt")
 	end
 end
 
 class Bishop < Piece # moves any number of vacant squares diagonally
 	def available_moves(x, y)
-		horz_left x, y
-		horz_right x, y
-		vert_up x, y
-		vert_down x, y
-		moves
+		IO.readlines("simple_moves.txt")
 	end
 end
 
 class Rook < Piece  # moves any number of vacant squares horizonally or vertically
 	def available_moves(x, y)
-		horz_left x, y
-		horz_right x, y
-		vert_up x, y
-		vert_down x, y
-		moves
+		IO.readlines("simple_moves.txt")
 	end
 end
 		 	
-class Queen < Piece
+class Queen < Piece  # moves any number of vacant squares horizonally, vertically, or diagnoally
 	def available_moves(x, y)
-	    horz_left @x, @y
-	    horz_right @x, @y
-	    vert_up @x, @y
-	    vert_down @x, @y
-	    moves # moves any number of vacant squares horizonally, vertically, or diagnoally
-		# def is_move_diagonal?	
-		# 	if @origin = @destination = 
-		# end
+		IO.readlines("simple_moves.txt")
 	end
 end
 
-class King < Piece
+class King < Piece # any one square any direction
 	def available_moves(x, y)
-	    horz_left @x, @y
-	    horz_right @x, @y
-	    vert_up @x, @y
-	    vert_down @x, @y
-	    moves
+		IO.readlines("simple_moves.txt")
 	end
 end
 
+class ChessValidator
+# 		converts [a, 1] to [0, 7]
+# 	def valid_move?
+# 		move [a, 1] to [a, 2]  #is move legal?
+	end
+# 	def array_of_moves
+# 		#is legal?
+# 		def moves
+# 			Array = #array of moves
 
-
-# # class ChessValidator
-# # 	initalize.Board.new #initializes board
-# # 	converts [a, 1] to [0, 7]
-# # 	def valid_move?
-# # 		move [a, 1] to [a, 2]  #is move legal?
-# # 	end
-# # 	def array_of_moves
-# # 		#is legal?
-# # 		def moves
-# # 			Array = #array of moves
-
+# my_game = Board.new
+# initalize.Board.new
 
 # # # #duplicated Rook and Queen class logic
 # # # 	#rook and queen should inherit from piece
